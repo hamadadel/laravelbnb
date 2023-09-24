7ccab78d8c350767ac6f6943bb229bbebf081b40
Hamad Adel <hamadwebdeveloper@gmail.com>
2023-09-25 00:27:13 +0300

    reinstall bootstrap and vue

--- resources/js/components/MahmoudAdel.vue
+++ resources/js/components/MahmoudAdel.vue
@@ -3,7 +3,7 @@
         <a
             href="https://medium.com/@DevMahmoudAdel/how-to-install-vue-3-in-laravel-10-with-vite-5c7749afd29c"
         >
-            How To Install Vue 3 in Laravel 10 : ZWebCourses :)
+            How To Install Vue 3 in Laravel 10 : ZWebCourses
         </a>
     </h1>
 </template>
