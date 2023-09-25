ea951fd9d10e5884a9d14aff0f902dd8751f6639
Hamad Adel <hamadwebdeveloper@gmail.com>
2023-09-25 18:21:33 +0300

    render Bookable component into Home component

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,3 +1,12 @@
 <template>
     <h1>Laravel Bnb with vue3</h1>
+    <Bookable />
 </template>
+
+<script>
+import Bookable from "./Bookable.vue";
+
+export default {
+    components: { Bookable },
+};
+</script>
