6e01da95406c5c90e4e1ad03ec77c17c16316254
Hamad Adel <hamadwebdeveloper@gmail.com>
2023-09-25 19:13:21 +0300

    explore Vue directives [v-for, v-if, v-else] in simple examples

--- resources/js/components/Bookable.vue
+++ resources/js/components/Bookable.vue
@@ -1,16 +1,15 @@
 <template>
     <div>
         <h2>{{ name }}</h2>
-        <BookableItem
-            :title="bookable1.title"
-            :content="bookable1.content"
-            :price="bookable1.price"
-        />
-        <BookableItem
-            :title="bookable2.title"
-            :content="bookable1.content"
-            :price="bookable1.price"
-        />
+        <p v-if="isLoading">Loading.....</p>
+        <div v-else>
+            <BookableItem
+                v-for="bookable in bookables"
+                :title="bookable.title"
+                :content="bookable.content"
+                :price="bookable.price"
+            />
+        </div>
     </div>
 </template>
 <script>
@@ -18,26 +17,27 @@ import BookableItem from "./BookableItem.vue";
 
 export default {
     created() {
-        console.log(this.bookable1);
-        console.log(this.bookable2);
-
         setTimeout(() => {
-            this.bookable1.title = "new apartment from the server";
-        }, 1500);
+            this.bookables = [
+                {
+                    title: "unique villa",
+                    content: "very cheap and unique villa",
+                    price: 1000,
+                },
+                {
+                    title: "elite apartment",
+                    content: "this apartment has a wonderful view",
+                    price: 1500,
+                },
+            ];
+            this.isLoading = false;
+        }, 2500);
     },
     data: function () {
         return {
             name: "Bookable container",
-            bookable1: {
-                title: "unique villa",
-                content: "very cheap and unique villa",
-                price: 1000,
-            },
-            bookable2: {
-                title: "elite apartment",
-                content: "this apartment has a wonderful view",
-                price: 1500,
-            },
+            bookables: null,
+            isLoading: true,
         };
     },
     components: {
