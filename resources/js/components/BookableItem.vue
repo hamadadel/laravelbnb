9bc7f1fde4808a0eeb86ae54dea704669f20456a
Hamad Adel <hamadwebdeveloper@gmail.com>
2023-09-25 18:20:59 +0300

    create BookableItem component which acts like a child in Bookable component, the create props and pathe them to child components and explore component state and reactivity

---
+++ resources/js/components/BookableItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h3>{{ title }}</h3>
+        <div>
+            {{ content }}
+            <span>{{ price }}</span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: { title: String, content: String, price: Number },
+};
+</script>
