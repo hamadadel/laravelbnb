a76367f7d4660b8b516ff093172621fd1d9b9271
Hamad Adel <hamadwebdeveloper@gmail.com>
2023-09-27 01:47:14 +0300

    create BookableFactory to generate some fake data for testing and BookableSeeder to persist the generated faker data into the database

---
+++ database/factories/BookableFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Bookable>
+ */
+
+
+class BookableFactory extends Factory
+{
+    private $suffix = [
+        'Villa',
+        'House',
+        'Cottage',
+        'Luxury Villas',
+        'Cheap House',
+        'Rooms',
+        'Cheap Rooms',
+        'Luxury Rooms',
+        'Fancy Rooms'
+    ];
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->city . ' ' . Arr::random($this->suffix),
+            'description' => $this->faker->text(),
+        ];
+    }
+}
