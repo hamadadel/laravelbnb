46e35952521b42a6e89e373f1b49a244f942fdb1
Hamad Adel <hamadwebdeveloper@gmail.com>
2023-09-29 18:19:09 +0300

    Install barryvdh/laravel-debugbar package and make new Booking Model which hols a list of booked  bookables

--- app/Models/Bookable.php
+++ app/Models/Bookable.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Bookable extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'description'];
+
+
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
 }
